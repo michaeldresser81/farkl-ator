d4314dddb6af50f71c6e999648418558fd592d96
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-01-08 08:30:56 -0500

    Document functions

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function roll(dice) {
+    // Rolls n six-sided dice, returns array of face values.
     const result = [];
     for (let i = 0; i < dice; i++) {
         result.push(Math.floor((Math.random() * 6)) + 1);
@@ -7,11 +8,13 @@ function roll(dice) {
 }
 
 function multiRoll(dice, rolls) {
+    // Rolls n dice, x times. Returns an object with the total
+    // of each resulting face value (1-6).
     const result = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0};
     for (let i = 0; i < rolls; i++) {
         const thisRoll = roll(dice);
-        thisRoll.forEach(die => {
-            result[die] += 1
+        thisRoll.forEach(number => {
+            result[number] += 1
         });
     }
     return result;
